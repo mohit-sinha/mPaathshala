40b5ae9c68a6232c043b69869206107f3e07ee2b
Mohit Sinha <msinha132@gmail.com>
2016-05-17 16:57:19 +0530

    finjava

--- app/src/main/java/com/example/mohit/mpaathshala/ShowLectDetails.java
+++ app/src/main/java/com/example/mohit/mpaathshala/ShowLectDetails.java
@@ -3,5 +3,38 @@ package com.example.mohit.mpaathshala;
 /**
  * Created by mohit on 17/5/16.
  */
-public class ShowLectDetails {
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class ShowLectDetails extends AppCompatActivity {
+
+    //Defining views
+    private TextView textViewLectname;
+    private TextView textViewLectsub;
+    private TextView textViewLectcom;
+    private TextView textViewLectlink;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_lect_details);
+
+        //Initializing Views
+        textViewLectname = (TextView) findViewById(R.id.textViewLectname);
+        textViewLectsub = (TextView) findViewById(R.id.textViewLectsub);
+        textViewLectcom = (TextView) findViewById(R.id.textViewLectcom);
+        textViewLectlink = (TextView) findViewById(R.id.textViewLectlink);
+
+        //Getting intent
+        Intent intent = getIntent();
+
+        //Displaying values by fetching from intent
+        textViewLectname.setText(String.valueOf(intent.getIntExtra(MainActivity.KEY_BOOK_ID, 0)));
+        textViewLectsub.setText(intent.getStringExtra(MainActivity.KEY_BOOK_NAME));
+        textViewLectcom.setText(intent.getStringExtra(MainActivity.KEY_BOOK_PRICE));
+        textViewLectlink.setText(String.valueOf(intent.getIntExtra(MainActivity.KEY_BOOK_STOCK,0)));
+    }
 }
