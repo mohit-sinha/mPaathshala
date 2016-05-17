dfefb7e0a89caf71aa25e5cfc96aaed295270965
Mohit Sinha <msinha132@gmail.com>
2016-05-17 16:01:06 +0530

    getset

---
+++ app/src/main/java/com/example/mohit/mpaathshala/Lect.java
@@ -0,0 +1,44 @@
+package com.example.mohit.mpaathshala;
+
+/**
+ * Created by mohit on 17/5/16.
+ */
+public class Lect {
+    private String name;
+    private String subject;
+    private String comment;
+    private String link;
+
+    //Getters and setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setPrice(String price) {
+        this.comment = comment;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+}
