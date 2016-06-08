f91d6256e30a1c0de4e1835adb513da162ade8c1
Mohit Sinha <msinha132@gmail.com>
2016-06-08 17:26:37 +0530

    new_json_new_retrofit

---
+++ app/src/main/java/com/example/mohit/mpaathshala/AndroidVersion.java
@@ -0,0 +1,22 @@
+package com.example.mohit.mpaathshala;
+
+/**
+ * Created by mohit on 8/6/16.
+ */
+public class AndroidVersion {
+    private String ver;
+    private String name;
+    private String api;
+
+    public String getVer() {
+        return ver;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getApi() {
+        return api;
+    }
+}
---
+++ app/src/main/java/com/example/mohit/mpaathshala/JSONResponse.java
@@ -0,0 +1,12 @@
+package com.example.mohit.mpaathshala;
+
+/**
+ * Created by mohit on 8/6/16.
+ */
+public class JSONResponse {
+    private AndroidVersion[] android;
+
+    public AndroidVersion[] getAndroid() {
+        return android;
+    }
+}
